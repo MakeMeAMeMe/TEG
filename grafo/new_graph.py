a94960592d189fa76c8610611d0435ba246ec82d
Gui-g <guilira94@gmail.com>
2020-03-16 20:58:34 -0300

    correção delete_vert, apresentação

--- grafo/new_graph.py
+++ grafo/new_graph.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from pprint import pprint
 
+#Parte 1: Implementação
+#A.1) matriz adjacência (grafo_1.txt)
 def create_matrix_adj(file_path):
     with Path(file_path).open(mode = "r") as graph_file:
         rows = graph_file.readlines()
@@ -17,15 +19,14 @@ def create_matrix_adj(file_path):
             matrix[i][j] = 1
     return matrix
 
+#A.2) matriz incidência (grafo_1.txt)
 def create_matrix_inc(file_path):
     with Path(file_path).open(mode = "r") as graph_file:
         rows = graph_file.readlines()
-    #pprint(rows)
     matrix = list()
     for i,row in enumerate(rows):
         row = row.strip()
         elements = row.split()
-        #pprint(elements)
         for element in elements:
             element = int(element)
             matrix.append(list())
@@ -38,6 +39,7 @@ def create_matrix_inc(file_path):
                 matrix[len(matrix)-1][element] = -1
     return matrix
 
+#B)Calcular grau de cada nó
 def calc_degree_vt_adj(matrix, vt, dir = True):
     count = 0
     for i in matrix[vt]:
@@ -50,12 +52,14 @@ def calc_degree_vt_adj(matrix, vt, dir = True):
 
     return count
 
+#Parte II.1: Inserir vertice (utilizando da matriz adjacência)
 def insert_vert(matrix):
     for i in range(len(matrix)):
         matrix[i].append(0)
     matrix.append(list())
     matrix[len(matrix)-1].extend([0 for i in range(len(matrix))])
 
+#Parte II.2: Deletar vertice (utilizando da matriz adjacência)
 def delete_vert(matrix, vert):
     new_matrix = list()
     for i in range(len(matrix)):
@@ -63,8 +67,13 @@ def delete_vert(matrix, vert):
             new_matrix.append(list())
             for j in range(len(matrix[i])):
                 if j != vert:
-                    new_matrix[i].append(matrix[i][j])
+                    if i > vert:
+                        new_matrix[i-1].append(matrix[i][j])
+                    else:
+                        new_matrix[i].append(matrix[i][j])
+    return new_matrix
 
+#Parte III: Matriz adjacência do complemento do grafo (utilizando da matriz adjência)
 def matrix_adj_compl_graph(matrix):
     ...
     compl_matrix = list()
@@ -78,21 +87,18 @@ def matrix_adj_compl_graph(matrix):
     
     return compl_matrix
 
-#matrix = create_matrix_adj()
-#create_matrix_inc()
-#pprint(calc_degree_vt_adj(matrix, 5))
-
-pprint("Matriz não direncionada")
+print("Parte 1 | B) Apresentar matriz adjacência e grau")
 matrix = create_matrix_adj("data/grafo_3.txt")
-insert_vert(matrix)
-compl = matrix_adj_compl_graph(matrix)
 pprint(matrix)
-pprint(compl)
-
-#for i in range(len(matrix)):
-#    pprint(calc_degree_vt_adj(matrix,i,False))
-##
-##pprint("Matriz direcionada")
-##matrix = create_matrix_adj("data/grafo_4.txt")
-#for i in range(len(matrix)):
-#    pprint(calc_degree_vt_adj(matrix,i))
+print()
+print("Grau dos vertices:")
+for i in range(len(matrix)):
+    print(i, ": ", calc_degree_vt_adj(matrix,i,False))
+print()
+print("Parte 1 | C) Grafo direcionado (arquivo: grafo_2.txt)")
+matrix = create_matrix_adj("data/grafo_4.txt")
+pprint(matrix)
+print()
+print("Grau dos vertices:")
+for i in range(len(matrix)):
+    print(i, ": ", calc_degree_vt_adj(matrix,i))
